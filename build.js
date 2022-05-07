4ff5444667708ad250424b0557e8689c9cb2ff74
fiatjaf <fiatjaf@gmail.com>
2022-05-07 08:30:20 -0300

    some changes in the build process.

--- build.js
+++ build.js
@@ -1,50 +1,41 @@
 #!/usr/bin/env node
 
 const esbuild = require('esbuild')
-const chokidar = require('chokidar')
-const liveServer = require('live-server')
 const alias = require('esbuild-plugin-alias')
 const nodeGlobals = require('@esbuild-plugins/node-globals-polyfill').default
+const liveServer = require('live-server')
 
 const prod = process.argv.indexOf('prod') !== -1
-const serve = process.argv.indexOf('serve') !== -1
 
-;(async () => {
-  const builder = await esbuild.build({
-    entryPoints: ['src/index.js'],
-    outfile: 'public/bundle.js',
-    bundle: true,
-    plugins: [
-      alias({
-        stream: require.resolve('readable-stream')
-      }),
-      nodeGlobals({buffer: true})
-    ],
-    define: {
-      window: 'self',
-      global: 'self'
-    },
-    loader: {'.js': 'jsx'},
-    sourcemap: prod ? false : 'inline',
-    minify: prod,
-    incremental: !prod
-  })
+const buildOptions = {
+  entryPoints: ['src/index.js'],
+  outfile: 'public/bundle.js',
+  bundle: true,
+  plugins: [
+    alias({
+      stream: require.resolve('readable-stream')
+    }),
+    nodeGlobals({buffer: true})
+  ],
+  define: {
+    window: 'self',
+    global: 'self'
+  },
+  loader: {'.js': 'jsx'},
+  sourcemap: prod ? false : 'inline',
+  minify: prod,
+  incremental: !prod
+}
 
-  if (serve) {
-    chokidar
-      .watch('src/**/*.{js,jsx}', {
-        interval: 0
-      })
-      .on('all', () => {
-        builder.rebuild()
-      })
-
+esbuild
+  .build(buildOptions)
+  .then(() => console.log('build success.'))
+  .then(() =>
     liveServer.start({
+      open: false,
+      port: 3000,
       host: 'localhost',
-      port: 7777,
-      root: 'public'
+      root: 'public',
+      file: 'index.html'
     })
-  } else {
-    console.log('build success.')
-  }
-})()
+  )
