13fb8c6cbc878d08ed6c7159d1f0c987c8badbcc
fiatjaf <fiatjaf@gmail.com>
2022-05-09 11:21:04 -0300

    esbuild server instead of live-server.

--- build.js
+++ build.js
@@ -3,9 +3,9 @@
 const esbuild = require('esbuild')
 const alias = require('esbuild-plugin-alias')
 const nodeGlobals = require('@esbuild-plugins/node-globals-polyfill').default
-const liveServer = require('live-server')
 
 const prod = process.argv.indexOf('prod') !== -1
+const watch = process.argv.indexOf('watch') !== -1
 
 const buildOptions = {
   entryPoints: ['src/index.js'],
@@ -27,15 +27,10 @@ const buildOptions = {
   incremental: !prod
 }
 
-esbuild
-  .build(buildOptions)
-  .then(() => console.log('build success.'))
-  .then(() =>
-    liveServer.start({
-      open: false,
-      port: 3000,
-      host: 'localhost',
-      root: 'public',
-      file: 'index.html'
-    })
-  )
+if (watch) {
+  esbuild
+    .serve({port: 7777, host: 'localhost', servedir: 'public'}, buildOptions)
+    .then(server => console.log(server))
+} else {
+  esbuild.build(buildOptions).then(() => console.log('build success.'))
+}
